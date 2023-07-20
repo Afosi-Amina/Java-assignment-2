04c144100beae9c38b4b46d70ff6265922fd5ec0
Afosi-Amina <136847293+Afosi-Amina@users.noreply.github.com>
2023-07-20 09:59:05 -0700

    Add files via upload

---
+++ meannumbers.java
@@ -0,0 +1,15 @@
+public class meannumbers{
+public static void main(String []mm){
+int a=1;
+int b=2;
+int c=3;
+int d=4;
+int e=5;
+int f=5;
+
+int result= (a+b+c+d+e)/f;
+
+System.out.print("answer="+result);
+}
+}
+
---
+++ times.java
@@ -0,0 +1,9 @@
+public class times{
+	public static void main(String []mm){
+		int d=4;
+		int e=5;
+		int f=6;
+		int result=d*e*f;
+		System.out.print("answer="+result);
+	}
+}
